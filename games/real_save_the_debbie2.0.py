934d608fcf281cf6b5a1bb514fcff60583c404ed
Senthan Bala <senthan.balavigneswaran@gmail.com>
2021-01-12 18:58:34 -0500

    finished!

--- games/real_save_the_debbie2.0.py
+++ games/real_save_the_debbie2.0.py
@@ -1,3 +1,5 @@
+from random import randint
+
 rooms = {
     "hall": {
         "items": [],
@@ -8,17 +10,42 @@ rooms = {
             "left": "king bed",
         },
     },
-    "attic": {"items": ["key"], "directions": {"down": "hall"}},
+    "attic": {"items": [], "directions": {"down": "hall"}},
     "basement": {"items": [], "directions": {"up": "hall"}},
-    "king bed": {"items": ["gun"], "directions": {"right": "hall"}},
+    "king bed": {
+        "items": [],
+        "directions": {"right": "hall", "left": "pantry"},
+    },
     "corridor": {
-        "items": ["monster"],
+        "items": [],
         "directions": {"right": "kid's bed", "left": "hall"},
     },
-    "kid's bed": {"items": ["debbie"], "directions": {"left": "corridor"}},
+    "kid's bed": {"items": [], "directions": {"left": "corridor"}},
+    "pantry": {"items": [], "directions": {"right": "king bed"}},
 }
 
+monsters = ["killer", "Nagini"]
+monster_room_list = ["king bed", "pantry", "corridor", "kid's bed"]
+all_items = ["sword", "gun", "key", "debbie"]
+item_room_list = ["attic", "pantry", "basement", "kid's bed"]
+
 is_p_in_maze = True
+
+
+def assign_monster(rooms, monster_room_list, monsters):
+    for i in range(len(monsters)):
+        m_room_index = randint(0, len(monster_room_list) - 1)
+        m_room = monster_room_list[m_room_index]
+        rooms[m_room]["items"].append(monsters[i])
+
+
+def assign_items(rooms, item_room_list, all_items):
+    for i in range(len(all_items) - 1, -1, -1):
+        i_room_index = randint(0, len(item_room_list) - 1)
+        i_room = item_room_list[i_room_index]
+        rooms[i_room]["items"].append(all_items[i])
+        all_items.remove(all_items[i])
+        item_room_list.remove(i_room)
 
 
 def ask_for_room(current_room, rooms):
@@ -45,14 +72,20 @@ def change_room(current_room, next_room_direction):
     return current_room
 
 
-def check_for_monster(current_room, rooms, inventory):
+def check_for_monsters(current_room, rooms, inventory):
     global is_p_in_maze
-    if "monster" in rooms[current_room]["items"] and "gun" not in inventory:
+    if "killer" in rooms[current_room]["items"] and "gun" not in inventory:
         is_p_in_maze = False
         print("The Monster got you! YOU LOSE!!!!")
-    elif "monster" in rooms[current_room]["items"] and "gun" in inventory:
-        print("You killed the monster! Yay!!!")
-        rooms[current_room]["items"].remove("monster")
+    elif "killer" in rooms[current_room]["items"] and "gun" in inventory:
+        print("You killed the killer! Yay!!!")
+        rooms[current_room]["items"].remove("killer")
+    if "Nagini" in rooms[current_room]["items"] and "sword" not in inventory:
+        is_p_in_maze = False
+        print("Nagini got you! YOU LOSE!!!!")
+    elif "Nagini" in rooms[current_room]["items"] and "sword" in inventory:
+        print("You killed Nagini! Yay!!!")
+        rooms[current_room]["items"].remove("Nagini")
 
 
 def ask_for_items(current_room, inventory, rooms):
@@ -70,7 +103,6 @@ def ask_for_items(current_room, inventory, rooms):
             else:
                 print("Sorry, that item is not in the room, try again.")
         else:
-            print("There are no items in the room")
             pick_up_available = False
     print("Your inventory: ", inventory)
 
@@ -82,12 +114,19 @@ def check_if_won(current_room, inventory):
         is_p_in_maze = False
 
 
+def assign(rooms, monster_room_list, monsters, item_room_list, all_items):
+    assign_monster(rooms, monster_room_list, monsters)
+    assign_items(rooms, item_room_list, all_items)
+
+
 current_room = "hall"
 inventory = []
 
+assign(rooms, monster_room_list, monsters, item_room_list, all_items)
+
 while is_p_in_maze:
     current_room = ask_for_room(current_room, rooms)
-    check_for_monster(current_room, rooms, inventory)
+    check_for_monsters(current_room, rooms, inventory)
     if is_p_in_maze == False:
         break
     ask_for_items(current_room, inventory, rooms)
