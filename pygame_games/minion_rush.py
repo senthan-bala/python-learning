7179d88383c7f7445f1e6aa157be409f04d9d48a
Senthan Bala <senthan.balavigneswaran@gmail.com>
2022-12-22 16:11:18 -0500

    made changes to pygame programs

--- pygame_games/minion_rush.py
+++ pygame_games/minion_rush.py
@@ -2,6 +2,8 @@ import pygame
 import os
 from random import randint
 
+# from random import choice
+
 pygame.font.init()
 pygame.mixer.init()
 
@@ -16,9 +18,15 @@ minion_sped = road_width + 5
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Minion Rush")
 # soundtrack = pygame.mixer.Sound(os.path.join("pygame_stuff", "minion_soundtrack.mp3"))
+font = pygame.font.Font("freesansbold.ttf", 15)
+end_font = pygame.font.Font("freesansbold.ttf", 70)
+well_done_font = pygame.font.Font("freesansbold.ttf", 35)
 
 
 def main_loop():
+    score = 0
+    obj_speed = 3
+    frames = 0
     start = False
     minion = pygame.Rect(
         (width / 2) - (minion_size / 2),
@@ -26,34 +34,61 @@ def main_loop():
         minion_size,
         minion_size,
     )
+    obj1 = pygame.Rect(130, -randint(400, 10000), minion_size - 10, minion_size - 10)
+    obj2 = pygame.Rect(185, -randint(400, 10000), minion_size - 10, minion_size - 10)
+    obj3 = pygame.Rect(240, -randint(400, 10000), minion_size - 10, minion_size - 10)
+    obj4 = pygame.Rect(130, -randint(400, 10000), minion_size - 10, minion_size - 10)
+    # obj5 = pygame.Rect(180, -randint(500, 700), minion_size - 15, minion_size - 15)
+    # obj6 = pygame.Rect(235, -randint(100, 300), minion_size - 15, minion_size - 15)
+    # obj_list = [obj1, obj2, obj3, obj4]
+    # if obj_speed >= 4:
+    obj_list = [obj1, obj2, obj3, obj4]
     road1 = pygame.Rect((width / 2) - 80, 0, road_width, height * 2)
     road2 = pygame.Rect((width / 2) - (road_width / 2), 0, road_width, height * 2)
     road3 = pygame.Rect((width / 2) + 30, 0, road_width, height * 2)
     road_base = pygame.Rect((width / 2) - 85, 0, (road_width * 3) + 20, height * 2)
     run = True
     while run:
+        current_score = font.render(str(score), True, (0, 0, 0), (255, 255, 255))
+        scorerect = current_score.get_rect()
+        scorerect.center = (50, 15)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
             if event.type == pygame.KEYDOWN:
-                # keys_pressed = pygame.key.get_pressed()
                 minion_movements(event, minion, road_base)
-        draw_screen(minion, road1, road2, road3, road_base)
+        run = detect_for_collision(minion, obj_list)
+        send_objects(obj_list, obj_speed, score)
+        draw_screen(
+            minion, road1, road2, road3, road_base, obj_list, scorerect, current_score
+        )
         if start == False:
-            start = True
             start = ask_to_start()
+        frames += 1
+        if frames == 9000:
+            frames -= 9000
+            obj_speed += 1
+        if run == False:
+            finish_game(score)
 
 
-def draw_screen(minion, road1, road2, road3, road_base):
+def draw_screen(
+    minion, road1, road2, road3, road_base, obj_list, scorerect, current_score
+):
     window.fill((255, 255, 255))
+    window.blit(current_score, scorerect)
     pygame.draw.rect(window, (0, 0, 0), road_base)
     pygame.draw.rect(window, road_color, road1)
     pygame.draw.rect(window, road_color, road2)
     pygame.draw.rect(window, road_color, road3)
     pygame.draw.rect(window, (255, 255, 0), minion)
-
+    [draw_objects(obj) for obj in obj_list]
     pygame.display.update()
+
+
+def draw_objects(obj):
+    pygame.draw.rect(window, (0, 0, 0), obj)
 
 
 def ask_to_start():
@@ -64,6 +99,7 @@ def ask_to_start():
         if output == "":
             print("Good Luck!")
             start = True
+            pygame.time.delay(2500)
             return start
         else:
             start = False
@@ -80,13 +116,48 @@ def minion_movements(event, minion, road_base):
         minion.x += minion_sped
 
 
-def send_objects():
-    obj1 = pygame.Rect(125, 50, minion_size, minion_size)
-    obj2 = pygame.Rect(180, 50, minion_size, minion_size)
-    obj3 = pygame.Rect(235, 50, minion_size, minion_size)
-    obj4 = pygame.Rect(125, 150, minion_size, minion_size)
-    obj5 = pygame.Rect(180, 150, minion_size, minion_size)
-    obj6 = pygame.Rect(235, 150, minion_size, minion_size)
+def send_objects(obj_list, obj_speed, score):
+    [send_obj(obj, obj_speed, score) for obj in obj_list]
+    obj1, obj2, obj3, obj4 = obj_list
+    if abs(obj2.y - obj3.y) <= 100 and (
+        abs(obj2.y - obj1.y) <= 100 or abs(obj2.y - obj4.y) <= 100
+    ):
+        obj2.y -= 200
+
+
+def send_obj(obj, obj_speed, score):
+    if obj.y <= height:
+        obj.y += obj_speed
+    elif obj.y > height:
+        obj.y = -randint(randint(100, 600), randint(700, 1500))
+        score += 100
+    return score
+
+
+def detect_for_collision(minion, obj_list):
+    keep_running = True
+    for obj in obj_list:
+        if minion.colliderect(obj):
+            keep_running = False
+    return keep_running
+
+
+def finish_game(score):
+    window.fill((255, 255, 255))
+    end_message = "Your final score was " + score
+    final_time = end_font.render(end_message, True, (0, 0, 0), (255, 255, 255))
+    textrect = final_time.get_rect()
+    textrect.center = (width / 2, height / 2)
+    window.blit(final_time, textrect)
+    if score >= 5000:
+        well_done_text = well_done_font.render(
+            "Well done!", False, (0, 0, 0), (255, 255, 255)
+        )
+        well_done = well_done_text.get_rect()
+        well_done.center = (width / 2, (height / 2) + 140)
+        window.blit(well_done_text, well_done)
+    pygame.display.update()
+    pygame.time.delay(4500)
 
 
 main_loop()
